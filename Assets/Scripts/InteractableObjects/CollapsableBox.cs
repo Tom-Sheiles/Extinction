df6fe00c470cce14977c972d5914704574baf11c
brendon250891 <brendon.willoughby@gmail.com>
2020-10-02 13:33:54 +1000

    Found another bug in loot generation.

--- Assets/Scripts/InteractableObjects/CollapsableBox.cs
+++ Assets/Scripts/InteractableObjects/CollapsableBox.cs
@@ -57,7 +57,7 @@ public class CollapsableBox : MonoBehaviour
     {
         GetComponent<Animation>().Play();
         isActive = false;
-        int itemToSpawn = Random.Range(0, possibleSpawns.Length);
+        int itemToSpawn = Random.Range(0, possibleSpawns.Length - 1);
         Debug.Log(itemToSpawn);
         if (itemToSpawn > 0)
         {
